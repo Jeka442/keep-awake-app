e38ad9be04e780525b6bd72743a60f0b16cc7423
Eivgeniy Gudilov <eivgeniygu@payoneer.com>
2025-01-23 11:16:29 +0200

    done

--- keep-awake.bat
+++ keep-awake.bat
@@ -2,11 +2,14 @@
 :: Your commands go here
 echo Checking if the directory exists...
 if exist "C:/keep-awake-app" (
+    echo directory exists, starting application
     cd C:\keep-awake-app
     npm start
 ) else (
+    echo directory dosn't exists, cloning from repo
     cd C:\
     git clone https://github.com/Jeka442/keep-awake-app.git
+    echo starting application
     cd C:\keep-awake-app
     npm i
     npm start
