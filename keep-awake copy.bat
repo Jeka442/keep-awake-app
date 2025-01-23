dfcac96ba7e77952819edd8e2cdef9fc679aeacd
Eivgeniy Gudilov <eivgeniygu@payoneer.com>
2025-01-23 11:11:57 +0200

    done

---
+++ keep-awake copy.bat
@@ -0,0 +1,15 @@
+@echo off
+:: Your commands go here
+echo Checking if the directory exists...
+if exist "C:/keep-awake-app" (
+    cd C:\keep-awake-app
+    npm start
+) else (
+    cd C:\
+    git clone https://github.com/Jeka442/keep-awake-app.git
+    cd C:\keep-awake-app
+    npm start
+)
+
+:: Keep the console open
+pause
